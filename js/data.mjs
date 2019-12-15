3e4687dfe769d7d06c2837d4eb4732c257efcda1
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2019-12-15 15:24:33 -0500

    edited some text

--- js/data.mjs
+++ js/data.mjs
@@ -2,12 +2,13 @@ export let posts = {
     //Blogs list
     blogs: [
         {
+            postedOn: "December 15 2019 Sunday",
             title: "Bienvenue",
             imageLocation: " ",
             content: `
             <img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1007&q=80" alt="paris-bridge welcome to blog" title="paris-bridge welcome to blog"/>
             <figcaption>Photo: par Leonard Cotte, Source: Unsplash.com, mercie beaucoup!</figcaption>
-            <p>Bienvenue tout le monde au ma premère site d'internet française! C'est ,a façon d'apprendre la nouvelle langue - par ècrire quelque chose touts les jours! Je pense que tout le monde peut apprendre quelconque langue! </p>
+            <p>Bienvenue tout le monde au ma premère site d'internet française! C'est ma façon d'apprendre la nouvelle langue - par ècrire quelque chose touts les jours! Je pense que tout le monde peut apprendre quelconque langue! La seule chose que nous doivons faire, c'est que nous ecrivons, et nous lisons, et, finelmment, nous écoutons quelque chose regulierement!</p>
             `
         },
     ],
