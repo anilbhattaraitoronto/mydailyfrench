c1e81a0efb38ab06794560dbabadb47bdf0463f3
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2019-12-15 15:07:31 -0500

    added a new french bloog

--- js/data.mjs
+++ js/data.mjs
@@ -7,6 +7,7 @@ export let posts = {
             content: `
             <img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1007&q=80" alt="paris-bridge welcome to blog" title="paris-bridge welcome to blog"/>
             <figcaption>Photo: par Leonard Cotte, Source: Unsplash.com, mercie beaucoup!</figcaption>
+            <p>Bienvenue tout le monde au ma premère site d'internet française! C'est ,a façon d'apprendre la nouvelle langue - par ècrire quelque chose touts les jours! Je pense que tout le monde peut apprendre quelconque langue! </p>
             `
         },
     ],
