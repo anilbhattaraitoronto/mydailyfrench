d3bcd5b92db6b26f6ee9b95337d182ea5df48530
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2019-12-15 14:18:24 -0500

    Second commit: Basic layout and blog and fweet display logics done.

--- js/app.mjs
+++ js/app.mjs
@@ -1,4 +1,5 @@
+import { posts } from '/js/data.mjs'
 let app = new Vue({
     el: "#root",
-    data: {}
+    data: posts
 })
---
+++ js/data.mjs
@@ -0,0 +1,20 @@
+export let posts = {
+    //Blogs list
+    blogs: [
+        {
+            title: "Bienvenue",
+            imageLocation: " ",
+            content: `
+            <img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1007&q=80" alt="paris-bridge welcome to blog" title="paris-bridge welcome to blog"/>
+            <figcaption>Photo: Leonard Cotte, Source: Unsplash.com Thanks!</figcaption>
+            `
+        },
+    ],
+    //Fweets List
+    fweets: [
+        {
+
+            content: "C'est ma prémiere fweet. Bienvenue "// short tweet-like snippet
+        }
+    ]
+}
